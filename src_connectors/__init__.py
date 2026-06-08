4ce9de71c0c897ee94e47b221e675a71ef6da1fb
CanhLD <canhld@vetc.com.vn>
2026-06-08 14:10:43 +0700

    Fix: init lib for src_connectors

--- src_connectors/__init__.py
+++ src_connectors/__init__.py
@@ -1,4 +1,18 @@
-from src_connectors.src_sqlserver.connector import SQLServerConnector
-from src_connectors.src_spark.connector import SparkConnector
+import sys
+from typing import Any
 
 __all__ = ["SQLServerConnector", "SparkConnector"]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "SQLServerConnector":
+        from src_connectors.src_sqlserver.connector import SQLServerConnector
+
+        return SQLServerConnector
+
+    if name == "SparkConnector":
+        from src_connectors.src_spark.connector import SparkConnector
+
+        return SparkConnector
+
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
